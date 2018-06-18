b20ae2bdd55c545b051f86ed402a972b3d9661d7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-06-18 11:37:55 -0400

    set up getVideo and paintToCanvas functions

--- scripts.js
+++ scripts.js
@@ -3,3 +3,24 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+}
+
+getVideo();
