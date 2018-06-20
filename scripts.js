37698886fc18ec57f0bffe2f36120f57b8228cfc
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-06-20 16:33:51 -0400

    set up redEffect function

--- scripts.js
+++ scripts.js
@@ -23,7 +23,14 @@ function paintToCanvas() {
   canvas.height = height;
 
   return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height)
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    pixels = redEffect(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+
   }, 16);
 
 }
@@ -38,8 +45,17 @@ function takePhoto() {
   const link = document.createeElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.textContext = 'Download Image';
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+  }
+  return pixels;
 }
 
 getVideo();
